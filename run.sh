6bacaa59827feb2c7e8dc14d04cc38ca19f9601f
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-05 21:51:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-# Virtual env banao agar nahi hai
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
-
-# Virtual env activate karo
 source venv/bin/activate
-
-# Dependencies install karo
 pip3 install -r requirements.txt
-
-# Gunicorn se API chala
-gunicorn --workers 1 --threads 4 --bind 0.0.0.0:8080 --timeout 600
+nohup gunicorn --workers 1 --threads 4 --bind 0.0.0.0:5000 --timeout 600
